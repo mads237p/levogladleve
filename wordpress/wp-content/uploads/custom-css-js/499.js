93293f4af445e7457fe4d91ab60739b637e505df
Oliver Kesi <oliverkesi@Olivers-MacBook-Pro-2.local>
2020-12-18 03:09:07 +0100

    Final project

---
+++ wordpress/wp-content/uploads/custom-css-js/499.js
@@ -0,0 +1,36 @@
+<!-- start Simple Custom CSS and JS -->
+<script type="text/javascript">
+
+
+var min_w = 300;
+var vid_w_orig;
+var vid_h_orig;
+
+
+$(document).ready(function($){
+
+  $(".comments-anchor").click(function () {
+     console.log("Ting sker");
+    window.scrollTo(0, 900);
+
+});
+
+
+  if ( $("body").hasClass("page-id-6") == true) {
+
+    console.log($(".page-id-6"));
+
+    $("video").show();
+
+  }
+
+    else {
+
+      $("video").hide();
+}
+
+
+
+});
+</script>
+<!-- end Simple Custom CSS and JS -->
